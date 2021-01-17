f38d4551804fefe533cf00f67b1275e58b513b1b
Rick <dirklo@gmail.com>
2021-01-17 17:48:13 +0100

    commit tree 459399cde3a2d4ed13739644195305ca7db68a48

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        first_name+" "+last_name
+    end
+
+    def list_roles
+        shows.collect do |show|
+            "#{show.characters.where(actor_id: id)[0].name} - #{show.name}" 
+        end
+    end
 end
